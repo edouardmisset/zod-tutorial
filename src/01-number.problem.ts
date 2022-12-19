9e872026334be071f58c8d3ac074e636df51a3a0
Edouard <63284636+edouardmisset@users.noreply.github.com>
2022-12-19 11:17:03 +0000

    Solved exercise 01

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,21 +1,19 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest'
+import { z } from 'zod'
 //       ^ 🕵️‍♂️
 
-export const toString = (num: unknown) => {
-  return String(num);
-};
+const numberParser = z.number()
+
+export const toString = (num: unknown) => String(numberParser.parse(num))
 
 // TESTS
 
-it("Should throw a runtime error when called with not a number", () => {
-  expect(() => toString("123")).toThrowError(
-    "Expected number, received string",
-  );
-});
+it('Should throw a runtime error when called with not a number', () => {
+  expect(() => toString('123')).toThrowError('Expected number, received string')
+})
 
-it("Should return a string when called with a number", () => {
-  expect(toString(1)).toBeTypeOf("string");
-});
+it('Should return a string when called with a number', () => {
+  expect(toString(1)).toBeTypeOf('string')
+})
