564e56d3bf9adc5161fa3e657752b87338187a25
Edouard <63284636+edouardmisset@users.noreply.github.com>
2022-12-19 11:22:27 +0000

    Solved exercise 02

--- src/02-object.problem.ts
+++ src/02-object.problem.ts
@@ -1,24 +1,26 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest'
+import { z } from 'zod'
 
-const PersonResult = z.unknown();
-//                   ^ 🕵️‍♂️
+const PersonResult = z.object({
+  name: z.string(),
+});
+
 
 export const fetchStarWarsPersonName = async (id: string) => {
   const data = await fetch(
-    "https://totaltypescript.com/swapi/people/" + id + ".json",
-  ).then((res) => res.json());
+    `https://totaltypescript.com/swapi/people/${id}.json`
+  ).then(res => res.json())
 
-  const parsedData = PersonResult.parse(data);
+  const parsedData = PersonResult.parse(data)
 
-  return parsedData.name;
-};
+  return parsedData.name
+}
 
 // TESTS
 
-it("Should return the name", async () => {
-  expect(await fetchStarWarsPersonName("1")).toEqual("Luke Skywalker");
-  expect(await fetchStarWarsPersonName("2")).toEqual("C-3PO");
-});
+it('Should return the name', async () => {
+  expect(await fetchStarWarsPersonName('1')).toEqual('Luke Skywalker')
+  expect(await fetchStarWarsPersonName('2')).toEqual('C-3PO')
+})
