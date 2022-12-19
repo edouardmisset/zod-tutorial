f3d397c3fe4065fe721e8f443870953e37dd85bd
Edouard <63284636+edouardmisset@users.noreply.github.com>
2022-12-19 11:30:02 +0000

    Solved exercise 04

--- src/04-infer.problem.ts
+++ src/04-infer.problem.ts
@@ -1,18 +1,19 @@
 // CODE
 
-import { z } from "zod";
+import { TypeOf, z } from 'zod'
 
 const StarWarsPerson = z.object({
   name: z.string(),
-});
+})
 
 const StarWarsPeopleResults = z.object({
   results: z.array(StarWarsPerson),
-});
+})
 
-const logStarWarsPeopleResults = (data: unknown) => {
-  //                                    ^ 🕵️‍♂️
-  data.results.map((person) => {
-    console.log(person.name);
-  });
-};
+const logStarWarsPeopleResults = (
+  data: TypeOf<typeof StarWarsPeopleResults>
+) => {
+  data.results.map(person => {
+    console.log(person.name)
+  })
+}
